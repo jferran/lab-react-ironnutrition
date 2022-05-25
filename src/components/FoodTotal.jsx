fde931d4f27b6ee5c29f7b572560d9157253e705
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-05-25 21:52:14 +0200

    code cleaning

--- src/components/FoodTotal.jsx
+++ src/components/FoodTotal.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 
 function FoodTotal({totalFood}) {
-    console.log("we have: ", totalFood)
   return (
     <div>
         <h2>Today's foods</h2>
-        {totalFood.map(element => {
-            return (
-                <p key={element.name + element.calories}>{element.quantity} {element.name} = {element.calories * element.quantity} </p>
-            )
+        {totalFood.map(element => 
             
-        })}
+                <p key={element.name + element.calories}>{element.quantity} {element.name} = {element.calories * element.quantity} </p>
+            
+            
+        )}
         <h3>Total: {totalFood.reduce((acc, element)=>{
             return acc+(element.calories * element.quantity)
         }, 0)} cal</h3>
